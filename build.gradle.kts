01bd151ce3c242ac0aba8b2f5f6e2ffde1494654
halulkin <halulkin@gmail.com>
2025-04-03 16:22:02 +0200

    build: add Spotless plugin for code formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,15 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.spotless)
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        kotlin {
+            target("src/**/*.kt")
+            ktlint()
+        }
+    }
 }
