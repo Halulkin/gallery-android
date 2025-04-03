5b1bd2cb738f7141f6a1fa31fe362a10b79df2dd
halulkin <halulkin@gmail.com>
2025-04-03 16:06:04 +0200

    build: update Android application configuration and add library plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,22 +2,17 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.convention.android.application)
 }
 
 android {
     namespace = "com.halulkin.gallery"
-    compileSdk = 35
-
     defaultConfig {
         applicationId = "com.halulkin.gallery"
-        minSdk = 28
-        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -40,7 +35,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
