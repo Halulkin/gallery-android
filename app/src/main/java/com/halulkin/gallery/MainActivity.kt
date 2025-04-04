9046c6a3dda6abf81f20def080b54c2dccbe718f
halulkin <halulkin@gmail.com>
2025-04-04 13:20:58 +0200

    feat: add Dagger Hilt support to MainActivity

--- app/src/main/java/com/halulkin/gallery/MainActivity.kt
+++ app/src/main/java/com/halulkin/gallery/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.halulkin.designsystem.theme.GalleryTheme
 import com.halulkin.gallery.navigation.MainNavigation
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
